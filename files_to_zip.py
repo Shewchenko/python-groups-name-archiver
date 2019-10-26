f08cfdf47361de9f935e6cfb6b553a35840e22b5
Shevchenko Oleg <shevchenko.oleg5@gmail.com>
2019-10-26 15:49:14 +0300

    create archive and add files

--- files_to_zip.py
+++ files_to_zip.py
@@ -1,37 +1,34 @@
 import glob
 import os
-# importing required modules 
-from zipfile import ZipFile 
+from os.path import isfile, join, splitext
+import zipfile
 
 #run
 # C:\Users\Shew\python-3.7.4-embed-amd64\python.exe C:\Users\Shew\files_to_zip.py
 # C:\Users\Shew\python-3.7.4-embed-amd64\python.exe .\files_to_zip.py
 
-# path = 'C:\\Users\\Shew\\'
 path = '.\\'
-pathDir = path + 'arthives\\'
+pathArcive = path + 'arthives\\'
 
 
-#files = [f for f in glob.glob(path + "**/*.txt", recursive=True)]
-# files = [f for f in glob.glob(path + 'test_ar\\' + "**/*.txt")]
+filesDir = [f for f in os.listdir(path) if isfile(join(path, f))] 
+for file in filesDir:
+    if '.py' in file or '.gitignore' in file:
+        filesDir.remove(file)
 
-# for f in files:
-    # print(f)
+for file in filesDir:
+    if '.zip' in file:
+        filesDir.remove(file)
+# print(filesDir) 
 
-files = []
-# r=root, d=directories, f = files
-for r, d, f in os.walk(path):
-    for file in f:
-        if '.txt' in file:
-            files.append(os.path.join(r, file))
-            # files.append(file)
-for f in files:
-    print(f)
+for file in filesDir:
+    name, exten = splitext(file)
+    nameArchive = path + name + ".zip"
 
-file_name = path + "my_python_files.zip"
-with ZipFile(file_name, 'w') as zip:
-    for f in files:
-        zip.write(path + f)
-    print('Extracting all the files now...')
-    zip.extractall() 
-    print('Done!')
+    targ = 'w'
+    if isfile(nameArchive):
+        targ = 'a'
+        
+    zip = zipfile.ZipFile(nameArchive,targ)
+    zip.write(path + file)
+    zip.close()
